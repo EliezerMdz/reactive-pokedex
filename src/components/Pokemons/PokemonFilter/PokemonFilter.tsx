e158994a20db12b86edd12cbb1d3e923e314466c
Eliezer Mendez <eliezermdz7@gmail.com>
2022-12-24 14:36:41 -0400

    New filter component

---
+++ src/components/Pokemons/PokemonFilter/PokemonFilter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// @ts-ignore
+import styles from "./PokemonFilter.css";
+
+interface Filters {}
+
+const PokemonsFilter: React.FC<{ filters: Filters }> = ({ filters }) => {
+  return <></>;
+};
+
+export default PokemonsFilter;
